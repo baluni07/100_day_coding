d745d2b20f50fab4b96f3e4ef116e3307446b46a
baluni07 <500090978@stu.upes.ac.in>
2024-08-30 23:22:05 +0530

    Day 7
    check whether no is abundant or not.

---
+++ Day 7/Abundant.java
@@ -0,0 +1,22 @@
+
+
+public class Abundant {
+    public static boolean isAbundantNumber(int n)
+    {
+        
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+ 
+        if (sum > n)   return true;
+        else return false;
+    }
+public static void main(String[] args) {
+    int n = 18;
+    System.out.println(isAbundantNumber(n));
+}
+    
+}
