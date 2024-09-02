37d079d61d06b8bcc0a4729d2625336382c848cd
baluni07 <500090978@stu.upes.ac.in>
2024-09-03 00:00:04 +0530

    Day 8
    Maximum Product Subarray in an Array

---
+++ Day 8/Maxproduct.java
@@ -0,0 +1,20 @@
+public class Maxproduct {
+        public int maxProduct(int[] nums) {
+            int prefix=1;
+            int suffix=1;
+            int maxi=0;
+            for ( int i=0;i<nums.length;i++)
+            {
+                if(prefix==0) prefix=0;
+                if(suffix==0) suffix=0;
+                prefix= prefix*nums[i];
+                suffix= suffix*nums[i];
+                maxi= Math.max(maxi, Math.max(prefix,suffix));
+             }
+             return maxi;
+        }
+    public static void main(String[] args) {
+        int nums[]= {1,-3,2,4,-7,3};
+        System.out.println(maxProduct(nums));
+    }
+    }
