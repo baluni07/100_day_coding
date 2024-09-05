b7b71dd5cb0976c360c09628241c618d6b102854
baluni07 <500090978@stu.upes.ac.in>
2024-09-05 23:16:16 +0530

    Update nextPermu.java

--- Day 8/nextPermu.java
+++ Day 8/nextPermu.java
@@ -1,7 +1,6 @@
 
-
 public class nextPermu {
-    public void nextPermutation(int[] nums) {
+    public static void nextPermutation(int[] nums) {
         int n= nums.length;
         int i= n-2;
         int last= n-1;
@@ -18,21 +17,25 @@ public class nextPermu {
 
         reverse(nums,i+1,last);
     }
-    void swap(int nums[], int i, int j)
+    static void swap(int nums[], int i, int j)
     {
         int t=nums[j];
         nums[j]= nums[i];
         nums[i]=t;
     }
-    void reverse(int nums[],int start,int end)
+    static void reverse(int nums[],int start,int end)
     {
         if(start<end)
         {
         swap(nums,start,end);
         start++;
-        end--;
+        end--;        } 
+
         }
-    }
-} {
+        public static void main(String[] args) {
+            int nums[]= {1,2,3};
+            nextPermutation(nums);
+            System.out.println(nums);
+        }
     
 }
